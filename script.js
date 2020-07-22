9ce4bd4bcd23e0253a65a618ec0f3357cc06f2c8
Palevoda <67462448+Palevoda@users.noreply.github.com>
2020-07-22 10:17:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+
+$('#card-1').mouseover(function () {
+    $('#sip-1').css({'display':'block'});
+    $('#lb1-1').css({'display':'block'});
+    $('#lb2-1').css({'display':'block'});
+    $('#bc2-1').css({'display':'block'});
+
+});
+$('#card-1').mouseout(function () {
+    $('#sip-1').css({'display':'none'}); 
+    $('#lb1-1').css({'display':'none'});   
+    $('#lb2-1').css({'display':'none'}); 
+    $('#bc2-1').css({'display':'none'});  
+});
+
+
+$('#card-2').mouseover(function () {
+    $('#sip-2').css({'display':'block'});
+    $('#lb1-2').css({'display':'block'});
+    $('#lb2-2').css({'display':'block'});
+    $('#bc2-2').css({'display':'block'});
+
+});
+$('#card-2').mouseout(function () {
+    $('#sip-2').css({'display':'none'}); 
+    $('#lb1-2').css({'display':'none'});   
+    $('#lb2-2').css({'display':'none'}); 
+    $('#bc2-2').css({'display':'none'});  
+});
+
+
+
+$('#card-3').mouseover(function () {
+    $('#sip-3').css({'display':'block'});
+    $('#lb1-3').css({'display':'block'});
+    $('#lb2-3').css({'display':'block'});
+    $('#bc2-3').css({'display':'block'});
+
+});
+$('#card-3').mouseout(function () {
+    $('#sip-3').css({'display':'none'}); 
+    $('#lb1-3').css({'display':'none'});   
+    $('#lb2-3').css({'display':'none'}); 
+    $('#bc2-3').css({'display':'none'});  
+});
+
+
+$('#card-4').mouseover(function () {
+    $('#sip-4').css({'display':'block'});
+    $('#lb1-4').css({'display':'block'});
+    $('#lb2-4').css({'display':'block'});
+    $('#bc2-4').css({'display':'block'});
+
+});
+$('#card-4').mouseout(function () {
+    $('#sip-4').css({'display':'none'}); 
+    $('#lb1-4').css({'display':'none'});   
+    $('#lb2-4').css({'display':'none'}); 
+    $('#bc2-4').css({'display':'none'});  
+});
+
+
+
+$('#card-5').mouseover(function () {
+    $('#sip-5').css({'display':'block'});
+    $('#lb1-5').css({'display':'block'});
+    $('#lb2-5').css({'display':'block'});
+    $('#bc2-5').css({'display':'block'});
+
+});
+$('#card-5').mouseout(function () {
+    $('#sip-5').css({'display':'none'}); 
+    $('#lb1-5').css({'display':'none'});   
+    $('#lb2-5').css({'display':'none'}); 
+    $('#bc2-5').css({'display':'none'});  
+});
+
+$('#card-6').mouseover(function () 
+{
+    $('#sip-6').css({'display':'block'});
+    $('#lb1-6').css({'display':'block'});
+    $('#lb2-6').css({'display':'block'});
+    $('#bc2-6').css({'display':'block'});
+
+});
+$('#card-6').mouseout(function () 
+{
+    $('#sip-6').css({'display':'none'}); 
+    $('#lb1-6').css({'display':'none'});   
+    $('#lb2-6').css({'display':'none'}); 
+    $('#bc2-6').css({'display':'none'});  
+});
+
+
